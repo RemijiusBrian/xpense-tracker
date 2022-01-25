633a1c5723b1bc203f164391a90068901a41cdee
Ridill <ridilldev@gmail.com>
2022-01-25 21:48:54 +0530

    Implemented Splash Screen

--- app/src/main/java/com/ridill/xpensetracker/core/ui/MainActivity.kt
+++ app/src/main/java/com/ridill/xpensetracker/core/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.ridill.xpensetracker.core.domain.model.AppTheme
@@ -19,6 +20,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             val viewModel: MainViewModel = hiltViewModel()
             val appTheme by viewModel.appTheme.collectAsState(initial = AppTheme.SYSTEM_DEFAULT)
