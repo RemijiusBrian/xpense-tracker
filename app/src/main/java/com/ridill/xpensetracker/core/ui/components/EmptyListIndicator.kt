6f72f83f9bd6f71988d4c0cebbc540a115c001c6
Ridill <ridilldev@gmail.com>
2022-02-06 16:43:23 +0530

    Custom Empty List Indicator

---
+++ app/src/main/java/com/ridill/xpensetracker/core/ui/components/EmptyListIndicator.kt
@@ -0,0 +1,57 @@
+package com.ridill.xpensetracker.core.ui.components
+
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.ridill.xpensetracker.core.ui.theme.SpacingExtraSmall
+import kotlinx.coroutines.delay
+
+@Composable
+fun EmptyListIndicator() {
+    Column {
+        val animationValues = (1..3).map { index ->
+            var offset by remember { mutableStateOf(0f) }
+
+            LaunchedEffect(Unit) {
+                delay(Delay * index)
+                animate(
+                    initialValue = 0f,
+                    targetValue = 12f,
+                    animationSpec = infiniteRepeatable(
+                        animation = tween(
+                            durationMillis = Duration,
+                            delayMillis = IntervalDuration
+                        ),
+                        repeatMode = RepeatMode.Reverse,
+                    )
+                ) { animatedValue, _ -> offset = animatedValue }
+            }
+            offset
+        }
+
+        (animationValues).forEach { animationValue ->
+            Box(
+                modifier = Modifier
+                    .offset(y = animationValue.dp)
+                    .width(24.dp)
+                    .height(4.dp)
+                    .clip(RoundedCornerShape(50))
+                    .background(MaterialTheme.colors.primary)
+            )
+            Spacer(modifier = Modifier.height(SpacingExtraSmall))
+        }
+    }
+}
+
+private const val Duration = 500
+private const val Delay = 100L
+private const val IntervalDuration = 3_000
