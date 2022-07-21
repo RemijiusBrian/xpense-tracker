f8fb850303350a5d797f7df7f940ccecb816a6eb
Remijius Brian <ridilldev@gmail.com>
2022-07-22 00:17:40 +0530

    Refactored test

--- app/src/test/java/dev/ridill/xpensetracker/core/data/preferences/FakePreferencesManager.kt
+++ app/src/test/java/dev/ridill/xpensetracker/core/data/preferences/FakePreferencesManager.kt
@@ -1,18 +1,41 @@
 package dev.ridill.xpensetracker.core.data.preferences
 
+import dev.ridill.xpensetracker.core.domain.model.AppTheme
 import dev.ridill.xpensetracker.core.domain.model.XTPreferences
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class FakePreferencesManager : XTPreferencesManager {
 
-    private var preferencesObj = XTPreferences(1000L)
-    private val preferencesFlow = flowOf(preferencesObj)
+    private var preferencesObj = MutableStateFlow(
+        XTPreferences(
+            expenditureLimit = 1000L,
+            theme = AppTheme.SYSTEM_DEFAULT,
+            useDynamicTheming = true,
+            balanceWarningPercent = 0.25f
+        )
+    )
+
+    private val preferencesFlow = preferencesObj.asStateFlow()
 
     override val preferences: Flow<XTPreferences>
         get() = preferencesFlow
 
     override suspend fun updateExpenditureLimit(limit: Long) {
-        preferencesObj = preferencesObj.copy(limit)
+        preferencesObj.update { it.copy(expenditureLimit = limit) }
+    }
+
+    override suspend fun updateAppTheme(theme: AppTheme) {
+        preferencesObj.update { it.copy(theme = theme) }
+    }
+
+    override suspend fun updateUseDynamicTheming(use: Boolean) {
+        preferencesObj.update { it.copy(useDynamicTheming = use) }
+    }
+
+    override suspend fun updateBalanceWarningPercent(value: Float) {
+        preferencesObj.update { it.copy(balanceWarningPercent = value) }
     }
 }
